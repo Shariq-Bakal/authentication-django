6cb009a6c41c9e160dfc90d32f24b37cac28b876
Shariq Firdous <shariq.f.bakal@gmail.com>
2025-03-06 13:52:54 -0700

    added token generation for login and registration

--- account/renderers.py
+++ account/renderers.py
@@ -5,6 +5,7 @@ class UserRenderer(renderers.JSONRenderer):
     charset = 'utf-8' #UTF-8 supports all characters (English, Arabic, Hindi, Chinese, etc.).
     #Prevents encoding errors when handling non-English text.
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        print(data,"data")
         response = ''
         if "ErrorDetail" in str(data):
             response = json.dumps({'errors':data}) # error detail is found in data it will throw an error. You can get this error when you do not provide email and password and you can see this error by priting serializers.errors in view
--- account/views.py
+++ account/views.py
@@ -7,14 +7,29 @@ from .serializers import UserRegisterSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
 
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user) #RefreshToken.for_user(user) creates a refresh token for the given user.
+    print(refresh)
+    print(user)
+    #This function generates authentication tokens (refresh & access tokens) for a user in a Django application using JWT (JSON Web Tokens).
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
 class RegisterUserAV(APIView):
     renderer_classes = [UserRenderer] # This way you are going to use renderer class. It should be used inside a class
     def post(self, request):
         #This UserRenderer will help in front end
         serializer = UserRegisterSerializer(data = request.data) # gets data from frontend and serialize it 
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            #we need to call token generation code here
+            token = get_tokens_for_user(user)
+            return Response({'token':token, 'msg':'user created successfully'}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
         
@@ -29,13 +44,12 @@ class UserLoginAV(APIView):
             password = serializer.data.get("password")
             user = authenticate(email=email, password = password) # it will check if there are users in database or not
             if user is not None: 
-                return Response({"msg":"Login success"}, status=status.HTTP_200_OK)
+                #generate token when user is valid 
+                token = get_tokens_for_user(user)
+                return Response({"token": token,"msg":"Login success"}, status=status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #Fix: Ensure a Response is Always Returned
-
         
-
 # Create your views here.
 # from .serializers import CustomUserSerializer, UserProfileSerializer
 # from rest_framework.generics import ListCreateAPIView
